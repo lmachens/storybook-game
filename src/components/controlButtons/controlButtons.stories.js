9828206ec6353784579080728ab066c57c18495f
slawoe <info@slawomirernst.de>
2020-07-31 15:08:41 +0200

    Add control Button Components

---
+++ src/components/controlButtons/controlButtons.stories.js
@@ -0,0 +1,9 @@
+import "./controlButtons.css";
+import { ControlButtons } from "./controlButtons";
+
+export default { title: "Components/controlButtons" };
+
+export const controlButtons = () => {
+  const OuterDIV = ControlButtons();
+  return OuterDIV;
+};
