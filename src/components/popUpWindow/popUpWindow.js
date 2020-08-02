5f5b28974df23946d20fa507f511e24ffa6d9233
JonasImm <jonas.imm@gmail.com>
2020-08-02 11:33:36 +0200

    Created popUpWindow, not implemented

---
+++ src/components/popUpWindow/popUpWindow.js
@@ -0,0 +1,24 @@
+import { createElement } from "../../utils/elements";
+import "./popUpWindow.css";
+
+export const popUpWindow = () => {
+  const popUpWindow__container = createElement("div", {
+    className: "popUpWindow__container",
+  });
+  const button = createElement("button", {
+    className: "popUp__button",
+    innerText: "OK",
+  });
+  const imgContainer = createElement("div", { className: "imgContainer" });
+
+  const popUp = createElement("div", { className: "popUp" });
+  const textGameOVer = createElement("p", { innerText: "GAME OVER" });
+  const textInf = createElement("p", { innerText: "YOU GOT INFECTED" });
+
+  popUpWindow__container.append(popUp);
+  popUpWindow__container.append(imgContainer);
+  popUp.append(textGameOVer);
+  popUp.append(textInf);
+  popUpWindow__container.append(button);
+  return popUpWindow__container;
+};
